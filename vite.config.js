04dd2c2922912425f9996da4994bb3ce078eaf3a
mustapha <mostaphaelghazal102030@gmail.com>
2025-11-01 22:47:10 +0100

    dep

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // ⚠️ Mets le NOM de ton repo ici
+  base: '/portfolio/', // ⚠️ ton dépôt GitHub s'appelle "portfolio"
 })
